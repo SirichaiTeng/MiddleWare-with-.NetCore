8b891cbe7aa6acf2752e25d20ec708417bbff6e6
SirichaiTeng <chainis.teon@gmail.com>
2024-09-09 16:58:18 +0700

    Add project files.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiddleWare_with_.NetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("getHello")]
+        public IActionResult GetHello()
+        {
+            string result = "hello";
+            return Ok(Newtonsoft.Json.JsonConvert.SerializeObject(result));
+        }
+    }
+}
---
+++ Models/AuthMiddleware.cs
@@ -0,0 +1,31 @@
+﻿namespace MiddleWare_with_.NetCore.Models
+{
+    public class AuthMiddleware
+    {
+        private readonly string _apikey;
+        private readonly RequestDelegate _next;
+
+        private const string API = "x-api-key";
+
+        public AuthMiddleware(RequestDelegate next,IConfiguration configuration) 
+        {
+            _apikey = configuration["apikey"];
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            //use HttpContext ถ้าใช้ servicesfilter จะใช้ (AuthorizationFilterContext context)
+            var apiKey = context.Request.Headers[API].FirstOrDefault();
+
+            if (apiKey != _apikey)
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync("Unauthorized");
+                return; 
+            }
+
+            await _next(context);
+        }
+    }
+}
